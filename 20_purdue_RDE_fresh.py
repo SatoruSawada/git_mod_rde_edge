6f8dbb46fe721d960596c71b0ada34d268463972
satorun <s.w.d.realize@gmail.com>
2021-10-18 22:27:05 +0900

    20211018_commit2

--- 20_purdue_RDE_fresh.py
+++ 20_purdue_RDE_fresh.py
@@ -199,24 +199,35 @@ def func_intercept(slope, point):
 
 
 
+### 同じやん，いつか統一して
+### こいつが一般版
+def func_cross_gas_dynamics(point1, point2, lambda1, lambda2): ### ごめん，くそみたいな名前で
+    x = ((point1[1]-point2[1])-(lambda2*point1[0]-lambda1*point2[0]))/(lambda1-lambda2)
+    y = point1[1] - lambda2 * point1[0] + lambda2 * x
+    return x, y
 
 
-
-
+### 同じやん，いつか統一して
 def func_eq17dot44_eq17dot45(point1, point2, lambda_plus, lambda_minus):
     x4 = ((point1[1]-point2[1])-(lambda_minus*point1[0]-lambda_plus*point2[0]))/(lambda_plus-lambda_minus)
     y4 = point1[1] - lambda_minus * point1[0] + lambda_minus * x4
     return x4, y4
 
+### よく理解していない
+### とりあえず vol.2 p.203 の例題にしたがって書いてみる
+def func_func_MEPC_theta3(theta1, theta2, point1, point2, lambda_12, eps=10e-6):
+    lambda_o = math.tan((theta1+theta2)/2.)
+    delta = 1.0
+    while delta >= eps:
+        x, y = func_cross_gas_dynamics(point1, point2, lambda_o,lambda_12)
+        theta = theta2 + (y-point2[1])/(point1[1]-point2[1]) * (theta1-theta2)
+        lambda_o_new = math.tan(theta)
+        delta = abs((lambda_o-lambda_o_new)/lambda_o)
+        lambda_o = lambda_o_new
+    return x, y, theta
 
-def func_func_MEPC_theta3(theta1, theta2):
-    
 
-    
-    return theta3
-
-
-
+### 同じやん，いつか統一して
 def func_eq17dot43_eq17dot46(point2, point4, lambda_12, lambda_o):
     x3 = ((point2[1]-point4[1])-(lambda_12*point2[0]-lambda_o*point4[0]))/(lambda_o-lambda_12)
     y3 = point2[1] - lambda_12 * point2[0] + lambda_12 * x3
--- test.py
+++ test.py
@@ -1,6 +1,37 @@
 import numpy as np
+import math
 
-array = np.zeros((2,6))
-print(array)
+### 同じやん，いつか統一して
+### こいつが一般版
+def func_cross_gas_dynamics(point1, point2, lambda1, lambda2): ### ごめん，くそみたいな名前で
+    x = ((point1[1]-point2[1])-(lambda2*point1[0]-lambda1*point2[0]))/(lambda1-lambda2)
+    y = point1[1] - lambda2 * point1[0] + lambda2 * x
+    return x, y
+
+### よく理解していない
+### とりあえず vol.2 p.203 の例題にしたがって書いてみる
+def func_func_MEPC_theta3(theta1, theta2, point1, point2, lambda_12, eps=10e-6):
+    lambda_o = math.tan((theta1+theta2)/2.)
+    delta = 1.0
+    while delta >= eps:
+        x, y = func_cross_gas_dynamics(point1, point2, lambda_o,lambda_12)
+        theta = theta2 + (y-point2[1])/(point1[1]-point2[1]) * (theta1-theta2)
+        lambda_o_new = math.tan(theta)
+        delta = abs((lambda_o-lambda_o_new)/lambda_o)
+        lambda_o = lambda_o_new
+    return x, y, theta
+
+
+theta1 = 18.191 / 360. *2.*np.pi
+theta2 = 16.422 / 360. *2.*np.pi
+x1 = 0.13146
+y1 = 0.040118
+x2 = 0.135683
+y2 = 0.037123
+lambda2 = -0.70921
+
+
+
+
 
 
